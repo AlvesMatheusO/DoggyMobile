981acb68362b461e0622ec7eda533cb6b10b9f7f
AlvesMatheusO <matheusalv.oliveira@gmail.com>
2024-04-15 13:02:50 -0300

    navbar ready

--- App/Navigations/tabNavigation.jsx
+++ App/Navigations/tabNavigation.jsx
@@ -4,6 +4,9 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../Screens/HomeScreen/HomeScreen.jsx";
 import AddFood from "../Screens/AddFoodScreen/AddFood.jsx";
 import ProfileScreen from "../Screens/ProfileScreen/ProfileScreen.jsx";
+import { Entypo,FontAwesome, FontAwesome6} from '@expo/vector-icons';
+
+
 
 const Tab = createBottomTabNavigator();
 
@@ -13,9 +16,35 @@ export default function TabNavigation() {
         <Tab.Navigator screenOptions={{
             headerShown: false
         }}>
-            <Tab.Screen name='home' component={HomeScreen} />
-            <Tab.Screen name='addFood' component={AddFood} />
-            <Tab.Screen name='profile' component={ProfileScreen} />
+
+            <Tab.Screen name='home' component={HomeScreen}
+            options={{
+                tabBarLabel:({color}) => (
+                    <Text style={{color:color, fontSize:12, marginTop: -7}}>Home</Text>
+                ),
+                tabBarIcon:({color, size}) => (
+                    <Entypo name="home" size={24} color="black" />
+                )
+            }} />
+            <Tab.Screen name='addFood' component={AddFood} 
+            options={{
+                tabBarLabel:({color}) => (
+                    <Text style={{color:color, fontSize:12, marginTop: -7}}>Adicionar Ração</Text>
+                ),
+                tabBarIcon:({color, size}) => (
+                    <FontAwesome6 name="bone" size={24} color="orange" />
+                )
+            }}/>
+      
+            <Tab.Screen name='profile' component={ProfileScreen}
+            options={{
+                tabBarLabel:({color}) => (
+                    <Text style={{color: color, fontSize: 12, marginTop: -7}}>Perfil</Text>
+                ),
+                tabBarIcon:({color, size}) => (
+                    <FontAwesome name="user-circle" size={24} color="black" />
+                )
+            }} />
         </Tab.Navigator>
     );
 }
