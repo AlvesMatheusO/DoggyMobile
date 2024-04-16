1e195f41166efaaa198c21589a51b3f86ce3c5d6
AlvesMatheusO <matheusalv.oliveira@gmail.com>
2024-04-16 11:35:42 -0300

    dog food historic

---
+++ App/Components/HistoricCard/historicCard.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Avatar, Button, Card, Text } from 'react-native-paper';
+import { ScrollView } from 'react-native';
+
+const HistoricCard = ({ foods }) => {
+
+    return (
+        // <View style={styles.container}>
+        //     <Text style={styles.title}>Histórico de Rações</Text>
+        //     {foods.map((food, index) => (
+        //         <View key={index} style={styles.card}>
+        //             <Text style={styles.cardTitle}>{food.brand}</Text>
+        //             <Text>{food.kg.$numberDecimal} Kg</Text>
+        //             <Text>R$ {food.price.$numberDecimal}</Text>
+        //             <Text>{food.date}</Text>
+        //             <Text style={styles.link}>Editar</Text>
+        //             <Text style={styles.link}>Deletar</Text>
+        //         </View>
+        //     ))}
+        // </View>
+
+        <ScrollView>
+            {console.log(foods)}
+            <Card>
+                <Card.Content>
+
+                    {foods.map((food, index) => (
+                        <View key={index}>
+                            <Text variant="titleLarge">{food.brand}</Text>
+                            <Text variant="bodyMedium">{food.kg.$numberDecimal} Kg</Text>
+                            <Text variant="bodyMedium">R$ {food.price.$numberDecimal}</Text>
+                            <Text variant="bodyMedium">{food.date}</Text>
+
+                            <Card.Actions>
+                                <Button>Cancel</Button>
+                                <Button>Ok</Button>
+                            </Card.Actions>
+
+                        </View>
+                    ))}
+
+                </Card.Content>
+
+            </Card>
+        </ScrollView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    card: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10,
+    },
+    cardTitle: {
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+    link: {
+        color: 'blue',
+        textDecorationLine: 'underline',
+        marginTop: 5,
+    },
+});
+
+export default HistoricCard;
--- App/Screens/HistoricScreen/historicScreen.jsx
+++ App/Screens/HistoricScreen/historicScreen.jsx
@@ -1,13 +1,58 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import Header from '../HomeScreen/Header';
+import api from '../../Services/api.js';
+import HistoricCard from '../../Components/HistoricCard/historicCard.jsx';
 
-function HitoricScreen() {
-  return (
-    <View>
-      <Text>Historic</Text>
-    </View>
-  )
+function HistoricScreen() {
+
+    const [brand, setBrand] = useState(null);
+    const [kg, setKg] = useState(null);
+    const [price, setPrice] = useState(null);
+
+    const [toggleModalDelete, setToggleModalDelete] = useState(false);
+    const [toggleModalEdit, setToggleModalEdit] = useState(false)
+
+    const [clicekdId, setClicekdId] = useState(null)
+    const id = clicekdId;
+
+    const [foods, setFoods] = useState([]);
+
+    const getFoods = async () => {
+
+        try {
+            const response = await api.get('/food');
+            const data = response.data;
+            setFoods(data);
+            console.log("conectou");
+
+        } catch (error) {
+            console.log(error);
+            alert("Não foi possivel carregador suas inserções, tente recarregar a página.");
+        }
+    };
+
+    useEffect(() => {
+        getFoods();
+    }, []);
+
+
+    return (
+        <View>
+            <View>
+                <Header />
+            </View>
+
+            <View>
+                <HistoricCard
+                    foods={foods}
+                    getFoods={getFoods} />
+
+                    
+            </View>
+        </View>
+    )
 }
 
 
-export default HitoricScreen;
+export default HistoricScreen;
