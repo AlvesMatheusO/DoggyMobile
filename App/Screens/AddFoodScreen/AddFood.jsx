10f0b1380ff1c216c912cffecd0899ba049bdb38
AlvesMatheusO <matheusalv.oliveira@gmail.com>
2024-05-15 17:47:52 -0300

    date picker to be fixed sender

--- App/Screens/AddFoodScreen/AddFood.jsx
+++ App/Screens/AddFoodScreen/AddFood.jsx
@@ -1,48 +1,90 @@
-import { View, Text, TextInput, StyleSheet, Alert } from 'react-native'
-import { Button } from 'react-native-paper';
-import React, { useState } from 'react'
-import api from '../../Services/api.js'
+import { View, Text, TextInput, StyleSheet, Alert, Button, Pressable, Platform } from 'react-native';
+import React, { useState } from 'react';
+import api from '../../Services/api.js';
+import DateTimePicker, { DateTimePickerAndroid } from '@react-native-community/datetimepicker';
 
-export default AddFood = () => {
-
+const AddFood = () => {
   const [brand, setBrand] = useState('');
   const [kg, setKG] = useState('');
   const [price, setPrice] = useState('');
+  const [date, setDate] = useState(new Date());
+  const [showPicker, setShowPicker] = useState(false);
+
+  const toggleDatePicker = () => {
+    setShowPicker(!showPicker);
+  };
+
+  const onChange = ({ type }, selectedDate) => {
+    if (type === "set") {
+      const currentDate = selectedDate || date;
+      setDate(currentDate);
+
+      if (Platform.OS === "android") {
+        toggleDatePicker();
+      }
+    } else {
+      toggleDatePicker();
+    }
+  };
+
+  const formatDate = (rawDate) => {
+    let date = new Date(rawDate);
+
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+
+    return `${day}/${month}/${year}`;
+  };
 
   const handlePost = async () => {
     const combinedText = brand + kg + price;
 
     if (containsAndroidEmoji(combinedText)) {
-      Alert.alert("Erro!", "Não é possível inserir ração com emojis do Android")
-    } else if (containsEmoji(combinedText)) {
-      Alert.alert("Erro!", "Não é possível inserir ração com emojis")
-    } else {
-      try {
-        if (parseFloat(kg) < 0) {
-          throw new Error('Não é permitido inserir números negativos para o peso.');
-        } else if (brand.length > 30) {
-          throw new Error('Não é permitido inserir mais que 30 caracteres na marca.');
-        } else if (!/^\d*\.?\d*$/.test(kg)) {
-          throw new Error('Apenas números são permitidos no campo de peso.');
-        } else if (!/^\d*\.?\d*$/.test(price)) {
-          throw new Error('Apenas números são permitidos no campo de preço.');
-        }
-
-        await api.post('/food', {
-          brand: brand,
-          kg: kg,
-          price: price
-        });
-        Alert.alert('Inserir Ração', 'Ração inserida com sucesso!', [
-          {
-            text: 'Sair'
-          }
-        ]);
-      } catch (error) {
-        Alert.alert('Erro!', error.message);
-      }
+      Alert.alert("Erro!", "Não é possível inserir ração com emojis do Android");
+      return;
     }
-  }
+
+    if (containsEmoji(combinedText)) {
+      Alert.alert("Erro!", "Não é possível inserir ração com emojis");
+      return;
+    }
+
+    try {
+      if (parseFloat(kg) < 0) {
+        throw new Error('Não é permitido inserir números negativos para o peso.');
+      }
+
+      if (brand.length > 30) {
+        throw new Error('Não é permitido inserir mais que 30 caracteres na marca.');
+      }
+
+      if (!/^\d*\.?\d*$/.test(kg)) {
+        throw new Error('Apenas números são permitidos no campo de peso.');
+      }
+
+      if (!/^\d*\.?\d*$/.test(price)) {
+        throw new Error('Apenas números são permitidos no campo de preço.');
+      }
+
+      const formattedDate = formatDate(date);
+
+      const response = await api.post('/food', {
+        brand,
+        date: formattedDate,
+        kg: parseFloat(kg),
+        price: parseFloat(price)
+      });
+
+      if (response.status === 200) {
+        Alert.alert('Inserir Ração', 'Ração inserida com sucesso!', [{ text: 'Sair' }]);
+      } else {
+        throw new Error('Erro ao inserir ração.');
+      }
+    } catch (error) {
+      Alert.alert('Erro!', error.message);
+    }
+  };
 
   const containsEmoji = (text) => {
     const emojiRegex = /[\u{1F600}-\u{1F64F}]/u;
@@ -54,10 +96,9 @@ export default AddFood = () => {
     }
 
     return false;
-  }
+  };
 
   const containsAndroidEmoji = (text) => {
-    // Lista de intervalos de código Unicode para emojis do Android
     const androidEmojiRanges = [
       [0x1F300, 0x1F3F0], // Miscellaneous Symbols and Pictographs
       [0x1F400, 0x1F640], // Emoticons
@@ -70,18 +111,14 @@ export default AddFood = () => {
       [0x1FA90, 0x1FAA8], // Flags
     ];
 
-    // Função para verificar se um código Unicode está dentro dos intervalos de emojis do Android
     const isAndroidEmoji = (char) => {
       return androidEmojiRanges.some(([start, end]) => {
         return char >= start && char <= end;
       });
     };
 
-    // Verifica cada caractere do texto
     for (let i = 0; i < text.length; i++) {
-      // Obtém o código Unicode do caractere
       const charCode = text.charCodeAt(i);
-      // Verifica se o caractere é um emoji do Android
       if (isAndroidEmoji(charCode)) {
         return true;
       }
@@ -91,40 +128,64 @@ export default AddFood = () => {
   };
 
   return (
-    <View>
-      <View style={styles.container}>
-        <View style={styles.title}>
-          <Text style={{ fontSize: 25 }}>Adicionar ração</Text>
-        </View>
-        <View style={styles.form}>
-          <Text style={styles.label}>Marca</Text>
-          <TextInput
-            style={styles.input}
-            value={brand}
-            onChangeText={(text) => setBrand(text)}
+    <View style={styles.container}>
+      <View style={styles.title}>
+        <Text style={{ fontSize: 25 }}>Adicionar ração</Text>
+      </View>
+
+      <View style={styles.form}>
+        <Text style={styles.label}>Marca</Text>
+        <TextInput
+          style={styles.input}
+          value={brand}
+          onChangeText={(text) => setBrand(text)}
+        />
+
+        <Pressable onPress={toggleDatePicker}>
+          <View style={styles.form}>
+            <Text style={styles.label}>Data</Text>
+            <TextInput
+              style={styles.input}
+              placeholder=' Insira a data de compra'
+              value={formatDate(date)}
+              placeholderTextColor="#FFF"
+              editable={false}
+            />
+          </View>
+        </Pressable>
+
+        {showPicker && (
+          <DateTimePicker
+            mode='date'
+            display='spinner'
+            value={date}
+            onChange={onChange}
           />
-          <Text style={styles.label}>Peso</Text>
-          <TextInput
-            style={styles.input}
-            value={kg}
-            onChangeText={(text) => setKG(text)}
-            keyboardType="numeric"
-          />
-          <Text style={styles.label}>Preço</Text>
-          <TextInput
-            style={styles.input}
-            value={price}
-            onChangeText={(text) => setPrice(text)}
-            keyboardType="numeric"
-          />
-        </View>
-        <View style={styles.button}>
-          <Button mode="contained" style={{ borderRadius: 20 }} onPress={handlePost}>Enviar</Button>
-        </View>
+        )}
+
+        <Text style={styles.label}>Peso</Text>
+        <TextInput
+          style={styles.input}
+          value={kg}
+          onChangeText={(text) => setKG(text)}
+          keyboardType="numeric"
+        />
+
+        <Text style={styles.label}>Preço</Text>
+        <TextInput
+          style={styles.input}
+          value={price}
+          onChangeText={(text) => setPrice(text)}
+          keyboardType="numeric"
+        />
+      </View>
+
+      <View style={styles.button}>
+        <Button title="Adicionar Ração" onPress={handlePost} />
       </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -147,4 +208,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#200fbab4',
     borderRadius: 10
   }
-})
+});
+
+export default AddFood;
